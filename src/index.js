44effcc29fac539ff6b2d012bd5fdbd5b50af2da
Hovappa96 <hovappakolkar@gmail.com>
2022-03-06 19:06:21 +0530

    assignment Updated

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
+const router = require('./routes/route.js');
+const moment = require("moment")
+const request = require("request-ip")
 const app = express();
+app.use(request.mw({attributeName:'myIp'}))
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,6 +18,20 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
+
+// const middleWare = function(req,res,next){
+// console.log("Middleware is Running Perfectly")
+// next()
+// }
+
+const middleWare = function (req,res,next){
+    let ip = req.myIp
+    console.log(moment().format("YYYY-MM-DD hh:mm:ss") +" "+ip+" "+req.originalUrl)
+    next()
+    }
+
+app.use(middleWare);
+
 
 app.use('/', route);
 
