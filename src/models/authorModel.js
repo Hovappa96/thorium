6ee8fbd5165872b8601d586d44bdf3172f481e26
Hovappa96 <hovappakolkar@gmail.com>
2022-03-12 20:34:53 +0530

    done

--- src/models/authorModel.js
+++ src/models/authorModel.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose")
 
-const authorSchema = new mongoose.Schema( {
-    author_id: String,
-    author_name: String,
-    age:Number,
-    address:String
 
-}, { timestamps: true });
+const authorSchema = new mongoose.Schema({
+        authorName:String,
+		age:Number,
+		address:String,
+        rating: Number
 
-module.exports = mongoose.model('Author', authorSchema)
+},{timeseries:true})
+
+module.exports= mongoose.model("author12",authorSchema)
--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -2,16 +2,19 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId
 
 const bookSchema = new mongoose.Schema( {
-    name: String,
-    author_id: {
-        type: ObjectId,
-        ref: "Author"
+    name:String,
+	author:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"author12"
     },
-    price: Number,
-    ratings: Number
-
+	price:Number,
+	ratings:Number,
+    publisher:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"publisher12"
+    }
 
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('LibraryBook', bookSchema)
+module.exports = mongoose.model('book12', bookSchema)
