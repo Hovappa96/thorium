f39d7c4df324ce0fc6974c828c36cc254335514a
Hovappa96 <hovappakolkar@gmail.com>
2022-03-21 13:07:05 +0530

    project completed

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -6,7 +6,7 @@ const createAuthor = async function (req, res) {
     try {
         let data = req.body;
         let savedata = await authorModel.create(data);
-        res.status(201).send({ status: true,msg:'Author is created successfully',msg: savedata })
+        res.status(201).send({ status: true,msg:'Author is created successfully',data: savedata })
     }
     catch (err) {
         res.status(500).send({ status: false, msg: err.message })
